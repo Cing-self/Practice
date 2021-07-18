a60640d1ab14d01f7f20df7a963864a171c4b92b
overclound <353674054@qq.com>
2021-07-18 17:08:13 +0800

    第一次提交

---
+++ src/TestMain.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class TestMain {
+    List<String> res = new ArrayList<>();
+
+    public static void  main(String[] args){
+        short a = 1;
+        short b = 2;
+        int c = a + b;
+    }
+
+}
